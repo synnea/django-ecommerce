fb590d3c1e682b4433d2a6a64448183d205a0cc0
Carina <carrie.poell@gmail.com>
2019-12-28 17:43:41 +0100

    set up postgres database.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2hdd$0swpwp%he7ea$t(6i0$8dcyxsi6(@kkc6iqu8l$n&q1g5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
